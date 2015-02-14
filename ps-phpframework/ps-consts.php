fb3f3f4bc49e402601bf663204c61aa4c4128f9e
prosource-jjung <contato@prosource.com.br>
2015-02-14 14:38:38 -0200

    Created by Coding Ground Client

---
+++ ps-phpframework/ps-consts.php
@@ -0,0 +1,29 @@
+<?php
+/*
+    ps-consts.php <Simple configuration for PS Framework>
+    
+    Copyright (C) 2015  ProSource Web Team: Jean Jung - Team leader.
+                            
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+const DEBUG = 1;
+
+const PS_ORM_SERVER_HOST = 'localhost';
+const PS_ORM_DB_NAME = 'CODINGGROUND';
+const PS_ORM_SCHEMA_NAME = 'CODINGGROUND';
+const PS_ORM_DB_USER = 'root';
+const PS_ORM_DB_PASSWORD = 'root';
+
+?>
